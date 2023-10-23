b6c7c66d7fc1dd64a0eb202b95226e3c2dc414c7
Wes <wesley.has@outlook.com>
2023-10-23 16:47:00 -0600

    removed old associations

--- models/index.js
+++ models/index.js
@@ -21,29 +21,4 @@ Pets.hasMany(Instructions, {
   onDelete: "CASCADE",
 });
 
-// Old asssociations that was not working
-/* User.hasMany(Pets, {
-  foreignKey: "user_id",
-  onDelete: "CASCADE",
-});
-
-Pets.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
-Pets.hasMany(Instructions, {
-  foreignKey: "pet_id",
-  onDelete: "CASCADE",
-});
-
-Instructions.belongsTo(Pets, {
-  foreignKey: "pet_id",
-}); */
-
-// Don't know what is this. It was here already.
-/*Pets.hasMany(Instructions, {
-  foreignKey: "pets_id",
-  onDelete: "CASCADE",
-});*/
-
 module.exports = { User, Instructions, Pets };
