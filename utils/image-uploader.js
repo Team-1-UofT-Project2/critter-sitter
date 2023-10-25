344cef44e410b34ce40c4a8e6b03e0b77833ca16
Wes <wesley.has@outlook.com>
2023-10-24 23:26:55 -0600

    added multer feat

--- utils/image-uploader.js
+++ utils/image-uploader.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 const filestorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, "images/");
+    //cb(null, "images/");
+    cb(null, "public/images/");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "--" + file.originalname);
@@ -11,11 +12,11 @@ const filestorageEngine = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true);
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
   } else {
-      //cb(null, false);
-      cb(new Error('upload correct file type'))
+    //cb(null, false);
+    cb(new Error("upload correct file type"));
   }
 };
 
@@ -23,9 +24,8 @@ const upload = multer({
   storage: filestorageEngine,
   limits: {
     fileSize: 1024 * 1024 * 10,
-    fileFilter:fileFilter
   },
-  
+  fileFilter: fileFilter,
 });
 
 module.exports = {
